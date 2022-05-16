b737a3def6ae74ba0ceb40293149be0b280a05f7
martinideniam <martinideniam@gmail.com>
2022-05-16 13:18:04 +0300

    Initial Commit

---
+++ weather-app-omo/weather_app_omoApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_omoApp.swift
+//  weather-app-omo
+//
+//  Created by Vladislav Gorovenko on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_omoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
